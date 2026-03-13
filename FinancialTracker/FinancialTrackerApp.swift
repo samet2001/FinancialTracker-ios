40b42c033d39de55afdca685031098c45f0bb605
samet2001 <samet44firinci@gmail.com>
2026-03-13 12:39:40 +0300

    Initial Commit

---
+++ FinancialTracker/FinancialTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancialTrackerApp.swift
+//  FinancialTracker
+//
+//  Created by SAMET FIRINCI on 13.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct FinancialTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
